d1ca78129be9c3de7fdb72b9f45e052b6157faf9
Dhruv Arora <98750226+lord-benjamin@users.noreply.github.com>
2023-07-04 03:16:49 +0530

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,41 @@
+const tabs = document.querySelectorAll(".tab");
+const panels = document.querySelectorAll(".panel");
+
+const btn = document.getElementById("menu-btn");
+const menu = document.getElementById("menu");
+const logo = document.getElementById("logo");
+
+tabs.forEach((tab) => tab.addEventListener('click',onTabClick))
+
+function onTabClick(e){
+    tabs.forEach((tab) => {
+        tab.children[0].classList.remove(
+            'border-softRed',
+            'border-b-4'
+        )
+    })
+
+    panels.forEach((panel) => panel.classList.add('hidden'))
+
+    e.target.classList.add('border-softRed','border-b-4');
+    const classString = e.target.getAttribute("data-target");
+    document.getElementById("panels").getElementsByClassName(classString)[0].classList.remove('hidden');
+}
+
+btn.addEventListener('click',navToggle);
+
+function navToggle(){
+    btn.classList.toggle('open');
+    menu.classList.toggle('flex');
+    menu.classList.toggle('hidden');
+
+    if(menu.classList.contains('flex')){
+        logo.setAttribute('src','./images/logo-bookmark-footer.svg');
+    }
+    else{
+        logo.setAttribute('src','./images/logo-bookmark.svg');
+    }
+}
+
+
+
